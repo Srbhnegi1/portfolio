3464024ed90ade4d2603957a848fa1fe158f087f
Saurabh Negi <srbhnegi333@gmail.com>
2024-07-04 22:21:19 +0530

    navMenuAdded

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,25 +1,38 @@
 import { NavLink } from "react-router-dom";
 import "./Header.css";
+import { HiOutlineXMark, HiOutlineBars4 } from "react-icons/hi2";
+import { useState } from "react";
+
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div>
       <div className="header">
-        <div>
-          <NavLink to="/">
-            <img
-              src="https://img.freepik.com/premium-vector/modern-letter-s-s-logo-monogram-logotype-vector-template-combined-with-luxury-fashion-identity_754537-1614.jpg?w=740"
-              alt="namelogo"
-              className="logo"
-            />
-          </NavLink>
+        <div className="header-top">
+          <div>
+            <NavLink to="/">
+              <img
+                src="https://img.freepik.com/premium-vector/modern-letter-s-s-logo-monogram-logotype-vector-template-combined-with-luxury-fashion-identity_754537-1614.jpg?w=740"
+                alt="namelogo"
+                className="logo"
+              />
+            </NavLink>
+          </div>
+          <div className="menu" onClick={handleMenuToggle}>
+            {menuOpen ? <HiOutlineXMark /> : <HiOutlineBars4 />}
+          </div>
         </div>
-        <div className="navLinks">
+        <div className={`navLinks ${menuOpen ? "open" : ""}`}>
           <div>
             <NavLink to="/about">
               <p>About</p>
             </NavLink>
           </div>
-
           <div>
             <NavLink to="/projects">
               <p>Projects</p>
